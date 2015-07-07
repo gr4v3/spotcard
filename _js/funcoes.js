f01faa6d86d6ba7511d9fed9b924b2c2e01edc2f
sergioaraujoadmin <sergiocaloiro@gmail.com>
2015-07-07 20:41:08 +0100

    Merge origin/master
    Conflicts:
    	index.html

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -356,38 +356,3 @@ function goTarget() {
             $('html,body').animate({scrollTop: top});
     });
 })();
-
-
-/*
- window.onload = function () {
- var wheelDistance = function (evt) {
- if (!evt)
- evt = event;
- var w = evt.wheelDelta, d = evt.detail;
- if (d) {
- if (w)
- return w / d / 40 * d > 0 ? 1 : -1; // Opera
- else
- return -d / 3;              // Firefox;         TODO: do not /3 for OS X
- } else
- return w / 120;             // IE/Safari/Chrome TODO: /3 for Chrome OS X
- };
- var wheelDirection = function (evt) {
- if (!evt)
- evt = event;
- return (evt.detail < 0) ? 1 : (evt.wheelDelta > 0) ? 1 : -1;
- };
- var test = document.getElementById('body');
- function showResults(evt) {
- var distance = wheelDistance(evt);
- var direction = wheelDirection(evt);
- }
- 
- if (test.addEventListener) {
- test.addEventListener('mousewheel', showResults, false);
- test.addEventListener('DOMMouseScroll', showResults, false);
- } else if (test.attachEvent) {
- test.attachEvent('onmousewheel', showResults);
- }
- }
- */
