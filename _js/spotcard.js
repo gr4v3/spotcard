808761b043c358ab07cf6ade2c1aa543d9c6a6e2
Fábio Menezes <fabio@navegabem.com>
2015-07-09 16:31:08 +0100

    Merge origin/master
    Conflicts:
    	_js/funcoes.js

--- _js/spotcard.js
+++ _js/spotcard.js
@@ -9,12 +9,22 @@ var spotcard = {
     root:'http://api.admedia.pt/',
     token:'c868be1151208dd4e5ebe7483022c1ed',
     img:'http://img.admedia.pt/img-medium/',
+    _response:function(content, callback, failback) {
+        var that = this;
+        if (content.status.value) {
+            for(index in content) {
+                console.log(typeof(content[index]));
+                //content[index] = that.htmlDecode(content[index]);
+            }
+            if (callback) callback(content);
+        } else if(failback) failback(content); else console.log(content);
+    },
     login:function(email,pass,callback, failback) {
+        var that = this;
         $.ajax({
             url:this.root + 'login/index/'+email+'/'+pass+'/sp'
         }).done(function(content) {
-            if (content.status.value && callback) callback(content);
-            else if(failback) failback(content); else console.log(content);
+            that._response(content, callback, failback);
         });
     },
     logout:function(callback, failback) {
@@ -24,8 +34,7 @@ var spotcard = {
             data: {token:that.token},
             url:this.root + 'login/out/'
         }).done(function(content) {
-            if (content.status.value && callback) callback(content);
-            else if(failback) failback(content); else console.log(content);
+            that._response(content, callback, failback);
         });
     },
     companies: function(category_id, callback, failback) {
@@ -35,8 +44,7 @@ var spotcard = {
             data: {token:that.token},
             url:this.root + 'companies/index/' + category_id
         }).done(function(content) {
-            if (content.status.value && callback) callback(content);
-            else if(failback) failback(content); else console.log(content);
+            that._response(content, callback, failback);
         });
     },
     categories: function(callback, failback) {
@@ -46,8 +54,7 @@ var spotcard = {
             data: {token:that.token},
             url:this.root + 'categories/index/'
         }).done(function(content) {
-            if (content.status.value && callback) callback(content);
-            else if(failback) failback(content); else console.log(content);
+            that._response(content, callback, failback);
         });
     },
     htmlDecode: function (value) {
