3232c1c41cb4fa4bd93c81ba463514301054663a
sergioaraujoadmin <sergiocaloiro@gmail.com>
2015-07-14 15:27:03 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	_css/content_aside_area.css
    	_css/content_aside_area2.css
    	_css/content_aside_area3.css
    	_css/custom.css
    	_css/flaticon/flaticon.css
    	_css/flaticon/flaticon.svg
    	_css/footer_area.css
    	_css/header_area.css
    	_css/menu.css
    	_css/nav_cartao/content_aside_area_c2.css
    	_css/nav_cartao/content_aside_area_c3.css
    	_css/service_area.css
    	_css/skeleton.css
    	_js/fixed.js
    	_js/funcoes.js
    	_js/modal.js
    	_js/spotcard.js
    	index.html
    	modal_login.html
    	nav_cartao/nav_home.html
    	nav_cartao/nav_home3.html
    	nbproject/project.properties
    	nbproject/project.xml

--- _js/site.js
+++ _js/site.js
@@ -6,111 +6,129 @@
 
 
 var site = {
-    show:{
-        login:function(email, pass) {
+    show: {
+        login: function (email, pass) {
             spotcard.login(email.replace('@', '%40'), pass, function (response) {
                 $('.window .close').click();
                 spotcard.token = response.user.token;
                 site.reset.categories();
-                $.get('templates/logout.mst', function(template) {
+                $.get('templates/logout.mst', function (template) {
                     var $user = $('.user');
-                        $user.html(Mustache.render(template, response));
+                    $user.html(Mustache.render(template, response));
                 });
             });
         },
-        companies:function(category_id) {
-            spotcard.companies(category_id, function(response) {
-                var $service_area = $('#service_area'); 
-                    $service_area.empty();
-                    response.name = response.items[0].category_id.name;
-                    $.get('templates/companies.mst', function(template) {
-                        $service_area.html(Mustache.render(template, response));
-                        var $subcategory = $service_area.find('#menu-subcategory');
-                            $subcategory.empty();
-                        var allsubcategories = [];
-                            response.items.forEach(function(element) {
-                                if (element.category_id.childs.forEach) 
-                                    element.category_id.childs.forEach(function(el) {
-                                        allsubcategories.push(el);
-                                    });
-                            }); 
-                        $.get('templates/subcategory.mst', function(template) {
-                                console.log(allsubcategories);
-                                var params = {'items' : allsubcategories};
-                                $subcategory.html(Mustache.render(template, params));
+        companies: function (category_id) {
+            spotcard.companies(category_id, function (response) {
+                var $service_area = $('#service_area');
+                $service_area.empty();
+                response.name = response.items[0].category_id.name;
+                $.get('templates/companies.mst', function (template) {
+                    $service_area.html(Mustache.render(template, response));
+                    var $subcategory = $service_area.find('#menu-subcategory');
+                    $subcategory.empty();
+                    var allsubcategories = [];
+                    response.items.forEach(function (element) {
+                        if (element.category_id.childs.forEach)
+                            element.category_id.childs.forEach(function (el) {
+                                allsubcategories.push(el);
                             });
-                        $service_area.find('select').each(function() {
-                            new SelectFx(this);
-                        });
                     });
+                    $.get('templates/subcategory.mst', function (template) {
+                        console.log(allsubcategories);
+                        var params = {'items': allsubcategories};
+                        $subcategory.html(Mustache.render(template, params));
+                    });
+                    $service_area.find('select').each(function () {
+                        new SelectFx(this);
+                    });
+                });
             });
         }
     },
-    reset:{
-        login:function() {
-            $.get('templates/login.mst', function(template) {
+    reset: {
+        login: function () {
+            $.get('templates/login.mst', function (template) {
                 var $user = $('.user');
-                    $user.html(template);
-                    $user.find('#login').click(function (e) {
+                $user.html(template);
+                $user.find('#login').click(function (e) {
+                    e.preventDefault();
+                    /* Lock BODY */
+                    $('body').on('wheel.modal mousewheel.modal', function () {
+                        return false;
+                    });
+
+                    var $id = $('#dialog');
+                    var maskHeight = $(document).height();
+                    var maskWidth = $(window).width();
+
+                    var $mask = $('#mask');
+                    $mask.css({'width': maskWidth, 'height': maskHeight});
+                    $mask.fadeIn(1000);
+                    $mask.fadeTo("slow", 0.8);	//opacity 80%
+
+                    //Get the window height and width
+                    var winH = $(window).height();
+                    var winW = $(window).width();
+
+                    $id.css('top', (winH / 2 - $id.height() / 2) - 20);
+                    $id.css('left', winW / 2 - $id.width() / 2);
+                    $id.fadeIn(2000);
+
+                    var form = document.getElementById('form_login');
+                    $(document).keyup(function (e) {
                         e.preventDefault();
-                        
-                        var $id = $('#dialog');
-                        var maskHeight = $(document).height();
-                        var maskWidth = $(window).width();
-
-                        var $mask = $('#mask');
-                            $mask.css({'width': maskWidth, 'height': maskHeight});
-                            $mask.fadeIn(1000);
-                            $mask.fadeTo("slow", 0.8);	//opacity 80%
-
-                        //Get the window height and width
-                        var winH = $(window).height();
-                        var winW = $(window).width();
-
-                        $id.css('top', winH / 2 - $id.height() / 2);
-                        $id.css('left', winW / 2 - $id.width() / 2);
-                        $id.fadeIn(2000);
-                        
-                        var form = document.getElementById('form_login');
-                            form.onsubmit = function () {
-                                site.show.login(this.elements.email.value, this.elements.password.value);
-                                return false;
-                            };
+                        if (e.keyCode == 13) {
+                            $(form).submit(); //enter
+                            //console.log("form.submit()");
+                        }
+                        if (e.keyCode == 27)
+                            $('.window .close').click();// esc
                     });
-                    site.reset.categories();
-                    $('.window .close').click(function (e) {
-                        e.preventDefault();
-                        $('#mask').hide();
-                        $('.window').hide();
-                    });
-                    $('#mask').click(function () {
-                        $(this).hide();
-                        $('.window').hide();
-                    });
+                    form.onsubmit = function () {
+                        site.show.login(this.elements.email.value, this.elements.password.value);
+                        return false;
+                    };
+                });
+                site.reset.categories();
+                $('.window .close').click(function (e) {
+                    e.preventDefault();
+                    $('#mask').hide();
+                    $('.window').hide();
+                    /* UnLock BODY */
+                    $('body').off('wheel.modal mousewheel.modal');
+                });
+                $('#mask').click(function () {
+                    $(this).hide();
+                    $('.window').hide();
+                    /* UnLock BODY */
+                    $('body').off('wheel.modal mousewheel.modal');
+                });
             });
         },
-        categories:function() {
-            spotcard.login('client%40admedia.pt', 'qwe123asd123', function(response) {
+        categories: function () {
+            spotcard.login('client%40admedia.pt', 'qwe123asd123', function (response) {
                 spotcard.token = response.user.token;
                 spotcard.categories(function (content) {
 
                     if (content.items) {
-                        var $service_area = $('#service_area'); 
-                            $service_area.empty();
+                        var $service_area = $('#service_area');
+                        $service_area.empty();
                         var div = document.createElement('div');
-                            div.className = 'container';
-                            $service_area.append(div);
+                        div.className = 'container';
+                        $service_area.append(div);
                         var $container = $(div);
-                        $.get('templates/category.mst', function(template) {
-                            content.items.forEach(function(item) {
-                                if (item.parent) return;
+                        $.get('templates/category.mst', function (template) {
+                            content.items.forEach(function (item) {
+                                if (item.parent)
+                                    return;
                                 item.img = spotcard.img + item.media_id.gallery_id.path + item.media_id.name;
                                 item.name = spotcard.htmlDecode(item.name);
                                 $container.append(Mustache.render(template, item));
                             });
                             //calculatePadding("#service_area");
-                        });    
-                    }     
+                        });
+                    }
                 });
             });
         }
