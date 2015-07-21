06e7f562716b53799a2643f8d49330da382d3ed9
sergioaraujoadmin <sergiocaloiro@gmail.com>
2015-07-21 18:15:42 +0100

    Merge origin/master
    Conflicts:
    	index.html

--- _js/site.js
+++ _js/site.js
@@ -4,48 +4,51 @@
  * and open the template in the editor.
  */
 window.isMobile = {
-    Android: function() {
+    Android: function () {
         return navigator.userAgent.match(/Android/i);
     },
-    BlackBerry: function() {
+    BlackBerry: function () {
         return navigator.userAgent.match(/BlackBerry/i);
     },
-    iOS: function() {
+    iOS: function () {
         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
-    Opera: function() {
+    Opera: function () {
         return navigator.userAgent.match(/Opera Mini/i);
     },
-    Windows: function() {
+    Windows: function () {
         return navigator.userAgent.match(/IEMobile/i);
     },
-    any: function() {
+    any: function () {
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
 
 var site = {
-    init:function(callback) {
+    init: function (callback) {
         console.log('site initialized!');
         spotcard.login('client%40admedia.pt', 'qwe123asd123', function (response) {
             spotcard.token = response.user.token;
-            if (callback) callback(response);
+            if (callback)
+                callback(response);
         });
     },
     show: {
-        banners:function() {
-            spotcard.banners(function(response) {
+        banners: function () {
+            spotcard.banners(function (response) {
                 response.source = spotcard.img;
                 $.get('templates/banner.mst', function (template) {
                     var banner_area = document.getElementById('banner_area');
-                        banner_area.innerHTML = Mustache.render(template, response);
-                        window.setInterval(function() {
-                            var current = banner_area.getElementsByClassName('current')[0];
-                            if (current.nextElementSibling) current.nextElementSibling.className = 'current';
-                            else banner_area.firstElementChild.className = 'current';
-                            current.className = '';
-                        }, 10000);
-                });    
+                    banner_area.innerHTML = Mustache.render(template, response);
+                    window.setInterval(function () {
+                        var current = banner_area.getElementsByClassName('current')[0];
+                        if (current.nextElementSibling)
+                            current.nextElementSibling.className = 'current';
+                        else
+                            banner_area.firstElementChild.className = 'current';
+                        current.className = '';
+                    }, 10000);
+                });
             });
         },
         login: function (email, pass) {
@@ -62,7 +65,8 @@ var site = {
         companies: function (category_id) {
             var $service_area = $('#service_area');
             spotcard.companies(category_id, function (response) {
-                if (!response.items.length) return;
+                if (!response.items.length)
+                    return;
                 $service_area.empty();
                 response.name = response.items[0].category_id.name;
                 response.category_id = response.items[0].category_id.category_id;
@@ -95,8 +99,35 @@ var site = {
                 });
 
             });
+
+            if (window.isMobile.any()) {
+                var $three = $('#service_area .content-aside_list_cat .three.columns');
+                var $toggle_itens_companies = $('#toggle_itens_companies');
+                var $nine = $('#service_area .content-aside_list_cat .nine');
+                var $discount = $('.content-aside_list_cat .nine .one:last-child');
+
+                $('#menu-subcategory > li').click(function () {
+                    if ($three.is(':visible')) {
+                        $three.hide("slide");
+                        $nine.addClass('width_100');
+                        $discount.show();
+                        $toggle_itens_companies.show();
+                    } else {
+                        $three.show("slide");
+                        $nine.removeClass('width_100');
+                        $discount.hide();
+                        $toggle_itens_companies.hide();
+                    }
+                });
+                $toggle_itens_companies.click(function () {
+                    $three.show();
+                    $nine.removeClass('width_100');
+                    $discount.hide();
+                    $(this).hide();
+                });
+            }
         },
-        company:function(client_id) {
+        company: function (client_id) {
             var $service_area = $('#service_area');
             spotcard.company(client_id, function (response) {
                 $.get('templates/company.mst', function (template) {
@@ -151,7 +182,7 @@ var site = {
                         site.show.login(this.elements.email.value, this.elements.password.value);
                         return false;
                     };
-                });     
+                });
                 $('.window .close').click(function (e) {
                     e.preventDefault();
                     $('#mask').hide();
@@ -165,94 +196,96 @@ var site = {
                     /* UnLock BODY */
                     $('body').off('wheel.modal mousewheel.modal');
                 });
-                
+
                 site.reset.categories();
             });
         },
         categories: function () {
             spotcard.categories(function (content) {
-                if (!content.items.length) return;
+                if (!content.items.length)
+                    return;
                 var $service_area = $('#service_area');
-                    $service_area.empty();
+                $service_area.empty();
                 var div = document.createElement('div');
-                    div.className = 'container';
-                    $service_area.append(div);
+                div.className = 'container';
+                $service_area.append(div);
                 var $container = $(div);
-                    $container.prepend( "<div class='responsive_item'><a href='#section2'>Pedir cartão</a></div>" );
+                $container.prepend("<div class='responsive_item'><a href='#section2'>Pedir cartão</a></div>");
                 $.get('templates/category.mst', function (template) {
                     content.items.forEach(function (item) {
                         item.img = spotcard.img + 'img-medium/' + item.media_id.gallery_id.path + item.media_id.name;
                         item.name = spotcard.htmlDecode(item.name);
                         $container.append(Mustache.render(template, item));
                     });
-                    $container.append( "<div class='responsive_item'><a href='#section3'>Contatos</a></div>");
+                    $container.append("<div class='responsive_item'><a href='#section3'>Contatos</a></div>");
                 });
                 $.get('templates/category_filter.mst', function (template) {
                     var $categoryfilter = $('.category-filter');
-                        $categoryfilter.html(Mustache.render(template, content));
+                    $categoryfilter.html(Mustache.render(template, content));
                     var select = $categoryfilter.find('select');
                     new SelectFx(select[0]);
                 });
             });
             spotcard.regions(function (content) {
-                if (!content.items.length) return;
+                if (!content.items.length)
+                    return;
                 $.get('templates/region_filter.mst', function (template) {
                     var $regionfilter = $('.region-filter');
-                        $regionfilter.html(Mustache.render(template, content));
+                    $regionfilter.html(Mustache.render(template, content));
                     var select = $regionfilter.find('select');
                     new SelectFx(select[0]);
                 });
             });
             site.show.banners();
             /*    
-            spotcard.regions(function (content) {
-                if (!content.items.length) return;
-                content.items.forEach(function (item) {
-
-                });
-
-                $.get('templates/region_filter.mst', function (template) {
-                    var $regionfilter = $('.region-filter');
-                        $regionfilter.html(Mustache.render(template, content));
-                    var select = $regionfilter.find('select');
-                    new SelectFx(select[0]);
-                    if (content.items) {
-                        var $service_area = $('#service_area');
-                        $service_area.empty();
-                        var div = document.createElement('div');
-                        div.className = 'container';
-                        $service_area.append(div);
-                        var $container = $(div);
-
-
-                        var divr = document.createElement('div');
-                        divr.className = 'responsive_item';
-                        var a = document.createElement('a');
-                        var linkText = document.createTextNode("Pedir cartão");
-                        a.appendChild(linkText);
-                        a.href = "#3rdPage";
-                        var $containerr = $(divr);
-                        $containerr.append(a);
-                        
-                        // $container.prepend( "<div class='responsive_item'><a href='#3rdPage'>Pedir cartão</a></div>" );
-                        $container.prepend($containerr);
-                        
-                        $.get('templates/category.mst', function (template) {
-                            content.items.forEach(function (item) {
-                                if (item.parent) return;
-                                if (item.media_id) {
-                                    item.img = spotcard.img + item.media_id.gallery_id.path + item.media_id.name;
-                                    item.name = spotcard.htmlDecode(item.name);
-                                    $container.append(Mustache.render(template, item));
-                                }
-                                
-                            });
-                            //calculatePadding("#service_area");
-                            $container.append( "<div class='responsive_item'><a href='#4thpage'>Contatos</a></div>");
-                        });
-                    }
-                });
-            });*/
+             spotcard.regions(function (content) {
+             if (!content.items.length) return;
+             content.items.forEach(function (item) {
+             
+             });
+             
+             $.get('templates/region_filter.mst', function (template) {
+             var $regionfilter = $('.region-filter');
+             $regionfilter.html(Mustache.render(template, content));
+             var select = $regionfilter.find('select');
+             new SelectFx(select[0]);
+             if (content.items) {
+             var $service_area = $('#service_area');
+             $service_area.empty();
+             var div = document.createElement('div');
+             div.className = 'container';
+             $service_area.append(div);
+             var $container = $(div);
+             
+             
+             var divr = document.createElement('div');
+             divr.className = 'responsive_item';
+             var a = document.createElement('a');
+             var linkText = document.createTextNode("Pedir cartão");
+             a.appendChild(linkText);
+             a.href = "#3rdPage";
+             var $containerr = $(divr);
+             $containerr.append(a);
+             
+             // $container.prepend( "<div class='responsive_item'><a href='#3rdPage'>Pedir cartão</a></div>" );
+             $container.prepend($containerr);
+             
+             $.get('templates/category.mst', function (template) {
+             content.items.forEach(function (item) {
+             if (item.parent) return;
+             if (item.media_id) {
+             item.img = spotcard.img + item.media_id.gallery_id.path + item.media_id.name;
+             item.name = spotcard.htmlDecode(item.name);
+             $container.append(Mustache.render(template, item));
+             }
+             
+             });
+             //calculatePadding("#service_area");
+             $container.append( "<div class='responsive_item'><a href='#4thpage'>Contatos</a></div>");
+             });
+             }
+             });
+             });*/
         }
     }
 };
